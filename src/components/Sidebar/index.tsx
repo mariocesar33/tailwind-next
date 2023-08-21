ccb7818f3de0eac3c25fac893c1160cfb1895f92
Mário César Silva <mcgato33@hotmail.com>
2023-08-21 05:52:20 -0100

    Widget de Espaço Usado

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,22 @@
-import { Search } from 'lucide-react'
+import { 
+  BarChart, 
+  CheckSquare, 
+  Cog, 
+  Flag, 
+  Home, 
+  LifeBuoy, 
+  Search, 
+  SquareStack, 
+  Users, 
+} from 'lucide-react'
 
+import { NavItem } from './NaveItem';
 import { Logo } from './Logo';
-import { MainNavigation } from './MainNavigation';
+import { UsedSpaceWidget } from './UsedSpaceWidget';
 
 export function SideBar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -13,7 +24,23 @@ export function SideBar() {
         <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search"/>
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
