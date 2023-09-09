a2cd7a486c78c635274bf8ee886aea384d2e3060
Mário César Silva <mcgato33@hotmail.com>
2023-09-09 12:59:12 -0100

    create SettingsTabs

---
+++ src/components/SettingsTabs/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import * as Tabs from '@radix-ui/react-tabs'
+
+export function SettingsTabs() {
+  return (
+    <Tabs.Root>
+      <Tabs.List className='mt-6 flex w-full items-center gap-4 border-b border-zinc-200'>
+        <Tabs.Trigger 
+          value='my-details' 
+          className='px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700'
+        >
+          <span className=''>My details</span>
+        </Tabs.Trigger>
+      </Tabs.List>
+    </Tabs.Root>
+  )
+}
